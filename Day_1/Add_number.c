872f4b7b399008a3ebb4167e85cdd4d63dd96964
Harsh sharma <harshsharma6349@gmail.com>
2024-12-28 16:39:20 +0530

    Day1 complete

---
+++ Day_1/Add_number.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main() {
+    int a = 10;
+    int b = 20;
+
+    printf("Sum of %d and %d is %d\n", a, b, a+b);
+    return 0;
+}
---
+++ Day_1/Ascii_print_using_getch.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+int main() {
+  char ch;
+  printf("Enter a character: ");
+    ch=getch();
+    printf("Ascii value of %c is %d\n", ch, ch);
+    return 0;
+}
